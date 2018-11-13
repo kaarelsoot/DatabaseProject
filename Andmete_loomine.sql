365ee5fd8168b829578ac6e13fe3224c74842ab6
joonaspraks <joonas.praks@profitsoftware.com>
2018-11-14 00:26:09 +0200

    Baas INSERT laused korras

--- Andmete_loomine.sql
+++ Andmete_loomine.sql
@@ -29,18 +29,16 @@ INSERT INTO "Auto_kategooria" (auto_kategooria_kood, nimetus, auto_kategooria_ty
 VALUES (1, 'Pereauto', 1), (2, 'Väikeauto', 1);
 
 INSERT INTO "Isik" (e_meil, isikukood, riik_kood, isiku_seisundi_liik_kood, synni_kp, parool, eesnimi, perenimi, elukoht)
-VALUES ('juhanmartens@cool.ee', '38711542746', 'EST', 'E', '12.01.1971', 'ajeeMaOlenParoolSuperKrypto8)', 'Juhan', 'Martens', 'Rapla');
-
-INSERT INTO "Isik" (e_meil, isikukood, riik_kood, isiku_seisundi_liik_kood, synni_kp, parool, eesnimi, perenimi, elukoht)
-VALUES ('klient@cool.ee', '38711542745', 'EST', 'E', '12.01.1970', 'ajeeMaOlenParoolSuperKrypto8)', 'Klient', 'Martens', 'Rapla');
+VALUES ('juhanmartens@cool.ee', '38711542746', 'EST', 'E', '12.01.1971', 'ajeeMaOlenParoolSuperKrypto8)', 'Juhan', 'Martens', 'Rapla'),
+('klient@cool.ee', '38711542745', 'EST', 'E', '12.01.1970', 'ajeeMaOlenParoolSuperKrypto8)', 'Klient', 'Martens', 'Rapla');
 
 INSERT INTO "Klient" (isik_id, kliendi_seisundi_liik_kood, on_nous_tylitamisega)
 VALUES (2, 'A', False);
 
 INSERT INTO "Tootaja" (isik_id, amet_kood, tootaja_seisundi_liik_kood)
-VALUES (1, 2, 'T')
+VALUES (1, 2, 'T');
 
--- INSERT INTO "Auto" (auto_kood, nimetus, vin_kood, auto_kytuse_liik_kood, auto_mark_kood, auto_seisundi_liik_kood, isik_id, mudel, valjalaseke_aasta, reg_number, istekohtade_arv, reg_aeg, mootori_maht)
--- VALUES (1234, 'Audi A6 2018', 'ABC123123123123', 1, 1, 1, )
+INSERT INTO "Auto" (auto_kood, nimetus, vin_kood, auto_kytuse_liik_kood, auto_mark_kood, auto_seisundi_liik_kood, isik_id, mudel, valjalaske_aasta, reg_number, istekohtade_arv, mootori_maht)
+VALUES (1234, 'Audi A6 2018', 'ABC123123123123', 1, 1, 1, 1, 'A6', 2018, '123ABC', 5, 2.8)
 
--- TODO: Pane default väärtused Kliendile ja Töötajale
+-- TODO: Lühenda Checki nimetusi, mis on liiga pikad (Muidu postgre ls lõikab nad väiksemaks)
