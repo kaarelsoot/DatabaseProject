91bfec7b4ea9d540fdd817321e51ad58f3f043fd
Azelor <kaarel.soot@gmail.com>
2018-12-04 12:26:39 +0200

    Lisatud funktsioonid auto lisamiseks ja unustamiseks

--- Andmebaasi ja andmete loomine/Funktsioonid.sql
+++ Andmebaasi ja andmete loomine/Funktsioonid.sql
@@ -1,45 +1,113 @@
-CREATE OR REPLACE VIEW aktiivsed_mitteaktiivsed_autod AS
-    SELECT auto.auto_kood, auto.nimetus, auto_seisundi_liik.nimetus, 
-    auto_mark.nimetus, auto.mudel, auto.valjalaske_aasta, auto.reg_number,
-    auto.vin_kood
-    FROM auto, auto_seisundi_liik, auto_mark
-    WHERE auto.auto_seisundi_liik_kood=auto_seisundi_liik.auto_seisundi_liik_kood AND
-    auto.auto_mark_kood=auto_mark.auto_mark_kood;
-COMMENT ON VIEW aktiivsed_mitteaktiivsed_autod IS '';
+--!--
+-- Auto margi sisetamine (seda probably vaja ei lähe - tegin, et harjutada funktsiooni kirjutamist)
+CREATE OR REPLACE FUNCTION f_lisa_auto_mark (
+  p_auto_mark_kood auto_mark.auto_mark_kood%TYPE, 
+  p_nimetus auto_mark.nimetus%TYPE) 
+  RETURNS auto_mark.auto_mark_kood%TYPE AS $$
+INSERT INTO auto_mark(auto_mark_kood, nimetus) VALUES
+(p_auto_mark_kood, p_nimetus) ON CONFLICT DO NOTHING
+RETURNING auto_mark_kood;
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
 
-CREATE OR REPLACE VIEW auto_kategooria_omamine_alamparing AS
-    SELECT auto.auto_kood, auto_kategooria.nimetus as kategooria
-    FROM auto, auto_kategooria, auto_kategooria_omamine
-    WHERE auto.auto_kood=auto_kategooria_omamine.auto_kood AND
-    auto_kategooria_omamine.auto_kategooria_kood=auto_kategooria.auto_kategooria_kood;
-COMMENT ON VIEW auto_kategooria_omamine_alamparing IS '';
+COMMENT ON FUNCTION f_lisa_auto_mark (
+  p_auto_mark_kood auto_mark.auto_mark_kood%TYPE, 
+  p_nimetus auto_mark.nimetus%TYPE) 
+  IS 'Selle funktsiooni abil saab sisestada uue auto margi';
+-- Näide kasutamisest
+SELECT f_lisa_auto_mark(p_auto_mark_kood:=7::SMALLINT, p_nimetus:='Toyota');
 
-CREATE OR REPLACE VIEW autode_detailid AS
-    SELECT auto.auto_kood, auto.nimetus, auto_mark.nimetus as mark, auto.mudel,
-    auto.valjalaske_aasta, auto.mootori_maht, auto_kytuse_liik.nimetus as kytus,
-    auto.istekohtade_arv, auto.reg_number, auto.vin_kood, auto.reg_aeg,
-    isik.eesnimi ||' '|| isik.perenimi ||' '|| isik.e_meil AS registreerija,
-    auto_seisundi_liik.nimetus as seisund
-    FROM auto, auto_seisundi_liik, auto_mark, auto_kytuse_liik, isik
-    WHERE auto.auto_seisundi_liik_kood=auto_seisundi_liik.auto_seisundi_liik_kood AND
-    auto.auto_mark_kood=auto_mark.auto_mark_kood AND
-    auto.auto_kytuse_liik_kood=auto_kytuse_liik.auto_kytuse_liik_kood AND
-    auto.lisaja_id=isik.isik_id;
-COMMENT ON VIEW autode_detailid IS '';
 
-CREATE OR REPLACE VIEW autode_koondaruanne AS
-    SELECT auto.auto_seisundi_liik_kood, 
-    auto_seisundi_liik.nimetus, COUNT(auto_seisundi_liik.nimetus) as hulk
-    FROM auto, auto_seisundi_liik
-    WHERE auto.auto_seisundi_liik_kood=auto_seisundi_liik.auto_seisundi_liik_kood
-    GROUP BY auto.auto_seisundi_liik_kood, auto_seisundi_liik.nimetus;
-COMMENT ON VIEW autode_koondaruanne IS '';
+--!--
+-- Auto registreerimine
+CREATE OR REPLACE FUNCTION f_lisa_auto (
+  p_auto_kood auto.auto_kood%TYPE,
+  p_nimetus auto.nimetus%TYPE,
+  p_vin_kood auto.vin_kood%TYPE,
+  p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
+  p_auto_mark_kood auto.auto_mark_kood%TYPE,
+  p_lisaja_id auto.lisaja_id%TYPE,
+  p_mudel auto.mudel%TYPE,
+  p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
+  p_reg_number auto.reg_number%TYPE,
+  p_istekohtade_arv auto.istekohtade_arv%TYPE,
+  p_mootori_maht auto.mootori_maht%TYPE) 
+  RETURNS auto.auto_kood%TYPE AS $$
+INSERT INTO auto(
+  auto_kood,
+  nimetus,
+  vin_kood,
+  auto_kytuse_liik_kood,
+  auto_mark_kood,
+  lisaja_id,
+  mudel,
+  valjalaske_aasta,
+  reg_number,
+  istekohtade_arv,
+  mootori_maht
+  ) VALUES (
+  p_auto_kood,
+  p_nimetus,
+  p_vin_kood,
+  p_auto_kytuse_liik_kood,
+  p_auto_mark_kood,
+  p_lisaja_id,
+  p_mudel,
+  p_valjalaske_aasta,
+  p_reg_number,
+  p_istekohtade_arv,
+  p_mootori_maht
+  ) ON CONFLICT DO NOTHING
+RETURNING auto_kood;
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
 
-CREATE OR REPLACE VIEW koik_autod AS
-    SELECT auto.auto_kood, auto.nimetus, auto_seisundi_liik.nimetus as seisund,
-    auto_mark.nimetus as mark, auto.mudel, auto.valjalaske_aasta,
-    auto.reg_number, auto.vin_kood
-    FROM auto, auto_seisundi_liik, auto_mark
-    WHERE auto.auto_seisundi_liik_kood=auto_seisundi_liik.auto_seisundi_liik_kood AND
-    auto.auto_mark_kood=auto_mark.auto_mark_kood
-COMMENT ON VIEW koik_autod IS '';
+-- Näide funktsiooni kasutamisest
+SELECT f_lisa_auto(
+  p_auto_kood:=3,
+  p_nimetus:='Volvo V70 2018',
+  p_vin_kood:='V123123123123',
+  p_auto_kytuse_liik_kood:=2::SMALLINT,
+  p_auto_mark_kood:=3::SMALLINT,
+  p_lisaja_id:=1,
+  p_mudel:='V70',
+  p_valjalaske_aasta:=2018::SMALLINT,
+  p_reg_number:='777VVV',
+  p_istekohtade_arv:=5::SMALLINT,
+  p_mootori_maht:=2.500
+);
+
+COMMENT ON FUNCTION f_lisa_auto (
+  p_auto_kood auto.auto_kood%TYPE,
+  p_nimetus auto.nimetus%TYPE,
+  p_vin_kood auto.vin_kood%TYPE,
+  p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
+  p_auto_mark_kood auto.auto_mark_kood%TYPE,
+  p_lisaja_id auto.lisaja_id%TYPE,
+  p_mudel auto.mudel%TYPE,
+  p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
+  p_reg_number auto.reg_number%TYPE,
+  p_istekohtade_arv auto.istekohtade_arv%TYPE,
+  p_mootori_maht auto.mootori_maht%TYPE)
+IS 'Selle funktsiooni abil saab sisestada uue auto';
+
+--!--
+-- Auto unustamine (peab olema seisundis 'O')
+CREATE OR REPLACE FUNCTION f_unusta_auto (
+  p_auto_kood auto.auto_kood%TYPE) 
+RETURNS VOID AS $$
+UPDATE auto SET auto_seisundi_liik_kood='U'
+WHERE p_auto_kood = auto.auto_kood
+AND auto_seisundi_liik_kood='O'
+RETURNING auto_seisundi_liik_kood
+;
+$$ LANGUAGE sql SECURITY DEFINER
+SET search_path = public, pg_temp;
+
+COMMENT ON FUNCTION f_unusta_auto (
+  p_auto_kood auto.auto_kood%TYPE)
+IS 'Selle funktsiooni abil saab auto unustada. 
+Eeltingimuseks on, et auto on seisundis Ootel';
+
+-- Näide funktsiooni kasutamisest
+SELECT f_unusta_auto(p_auto_kood:=3);
