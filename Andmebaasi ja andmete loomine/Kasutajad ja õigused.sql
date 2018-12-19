dc41b27fbf7c85b8e024fbb7233fee80ee10b449
Azelor <kaarel.soot@gmail.com>
2018-12-19 22:55:21 +0200

    Nihutatud asju kasutajate failis ja eemaldatud  vanad funktsioonid

--- Andmebaasi ja andmete loomine/Kasutajad ja õigused.sql
+++ Andmebaasi ja andmete loomine/Kasutajad ja õigused.sql
@@ -4,51 +4,42 @@ REVOKE CONNECT, TEMP ON DATABASE t154838 FROM PUBLIC;
 REVOKE CREATE, USAGE ON SCHEMA public FROM PUBLIC;
 REVOKE USAGE ON LANGUAGE plpgsql FROM PUBLIC;
 
-REVOKE EXECUTE ON FUNCTION
- f_registreeri_auto (
-  p_auto_kood auto.auto_kood%TYPE,
-  p_nimetus auto.nimetus%TYPE,
-  p_vin_kood auto.vin_kood%TYPE,
-  p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
-  p_auto_mark_kood auto.auto_mark_kood%TYPE,
-  p_lisaja_id auto.lisaja_id%TYPE,
-  p_mudel auto.mudel%TYPE,
-  p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
-  p_reg_number auto.reg_number%TYPE,
-  p_istekohtade_arv auto.istekohtade_arv%TYPE,
-  p_mootori_maht auto.mootori_maht%TYPE),
- f_unusta_auto (
-  p_auto_kood auto.auto_kood%TYPE),
- f_aktiveeri_auto (
-  p_auto_kood auto.auto_kood%TYPE),
- f_muuda_auto_mitteaktiivseks (
-  p_auto_kood auto.auto_kood%TYPE),
- f_muuda_auto (
-  p_auto_kood_vana auto.auto_kood%TYPE,
-  p_auto_kood_uus auto.auto_kood%TYPE,
-  p_nimetus auto.nimetus%TYPE,
-  p_vin_kood auto.vin_kood%TYPE,
-  p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
-  p_auto_mark_kood auto.auto_mark_kood%TYPE,
-  p_mudel auto.mudel%TYPE,
-  p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
-  p_reg_number auto.reg_number%TYPE,
-  p_istekohtade_arv auto.istekohtade_arv%TYPE,
-  p_mootori_maht auto.mootori_maht%TYPE),
- f_tuvasta_autode_haldur(
-  p_e_meil text, 
-  p_parool text),
- f_on_klient(
-  p_e_meil text, 
-  p_parool text),
- f_on_tootaja(
-  p_e_meil text, 
-  p_parool text),
- f_on_kasutaja(
-  p_e_meil text, 
-  p_parool text),
- f_lopeta_auto (
-  p_auto_kood auto.auto_kood%TYPE)
+REVOKE EXECUTE ON FUNCTION 
+f_registreeri_auto (
+    p_auto_kood auto.auto_kood%TYPE,
+    p_nimetus auto.nimetus%TYPE,
+    p_vin_kood auto.vin_kood%TYPE,
+    p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
+    p_auto_mark_kood auto.auto_mark_kood%TYPE,
+    p_lisaja_id auto.lisaja_id%TYPE,
+    p_mudel auto.mudel%TYPE,
+    p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
+    p_reg_number auto.reg_number%TYPE,
+    p_istekohtade_arv auto.istekohtade_arv%TYPE,
+    p_mootori_maht auto.mootori_maht%TYPE),
+f_unusta_auto (
+    p_auto_kood auto.auto_kood%TYPE),
+f_aktiveeri_auto (
+    p_auto_kood auto.auto_kood%TYPE),
+f_muuda_auto_mitteaktiivseks (
+    p_auto_kood auto.auto_kood%TYPE),
+f_muuda_auto (
+    p_auto_kood_vana auto.auto_kood%TYPE,
+    p_auto_kood_uus auto.auto_kood%TYPE,
+    p_nimetus auto.nimetus%TYPE,
+    p_vin_kood auto.vin_kood%TYPE,
+    p_auto_kytuse_liik_kood auto.auto_kytuse_liik_kood%TYPE,
+    p_auto_mark_kood auto.auto_mark_kood%TYPE,
+    p_mudel auto.mudel%TYPE,
+    p_valjalaske_aasta auto.valjalaske_aasta%TYPE,
+    p_reg_number auto.reg_number%TYPE,
+    p_istekohtade_arv auto.istekohtade_arv%TYPE,
+    p_mootori_maht auto.mootori_maht%TYPE),
+f_tuvasta_juhataja(
+    p_e_meil text, 
+    p_parool text),
+f_lopeta_auto (
+    p_auto_kood auto.auto_kood%TYPE)
 FROM PUBLIC;
 
 REVOKE EXECUTE ON ALL FUNCTIONS IN SCHEMA public FROM PUBLIC;
@@ -67,11 +58,11 @@ autode_koondaruanne
 TO juhataja_t154838;
 
 GRANT EXECUTE ON FUNCTION
-f_on_kasutaja(
-  p_e_meil text, 
-  p_parool text),
+f_tuvasta_juhataja(
+    p_e_meil text, 
+    p_parool text),
 f_lopeta_auto (
-  p_auto_kood auto.auto_kood%TYPE)
+    p_auto_kood auto.auto_kood%TYPE)
 TO juhataja_t154838;
 
 ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON FUNCTIONS FROM PUBLIC;
